f7f05ab218ffc8da91e0b12a553f232e8813e58d
Rajesh Adeli <rajannaadeli@gmail.com>
2023-03-06 15:06:11 +0530

    Adding some comments

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-
+//This is for to find the number of drum kits in the screen
 let numberofdrums = document.querySelectorAll(".drum").length;
 
+//This is for to add click event listener to the all drum kits(block). 
 for (let i = 0; i < numberofdrums; i++) {
     document.querySelectorAll(".drum")[i].addEventListener('click', function () {
+        //This is for to play the sound and to add some hover effect to the drums when someone clicks on the drum-kit
         sound(this.innerHTML);
         hovereffect(this.innerHTML);
-
     })
 }
+
+//This is for to add keypress event listener to the all drum kits(block), to play the sound when someone clicks on the specified keys on the keyboard.
 document.addEventListener("keypress", function (event) {
     event = event.key;
     sound(event);
     hovereffect(event);
 })
 
+//This is for to play corrent sound according to the block of drum kit.
 function sound(event) {
     let tom1 = new Audio('sounds/tom-1.mp3')
     let tom2 = new Audio('sounds/tom-2.mp3');
@@ -49,7 +53,7 @@ function sound(event) {
     }
 }
 
-
+// this is for to add the hover effect to the blocks.
 function hovereffect(event) {
     document.querySelector("." + event).classList.add("hoverEffect");
     setTimeout(() => {
